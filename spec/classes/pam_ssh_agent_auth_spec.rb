bedf8993f81c2142771f9cdd74d9d73311a872e9
Lutz Behnke <lutz.behnke@informatik.haw-hamburg.de>
2014-04-15 16:32:43 +0200

    add testing environment, dependency information

---
+++ spec/classes/pam_ssh_agent_auth_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe 'pam_ssh_agent_auth' do
+  let(:hiera_config) { 'spec/fixtures/hiera/hiera.yaml' }
+
+  context 'with default settings' do
+    it do
+      should contain_package('pam-ssh-agent-auth')
+
+      should contain_file('/home_local').with_ensure('directory')
+
+      should contain_file('/home_local/ubuntu').with_ensure('directory')
+
+      should contain_group('ubuntu')
+      should contain_user('ubuntu')
+      
+      should contain_file('/etc/sudoers')
+      
+      should contain_file('/home_local/ubuntu/.ssh').with_ensure('directory')
+      should contain_file('/home_local/ubuntu/.ssh/authorized_keys').with(
+      		'owner' => 'ubuntu',
+      		'group' => 'ubuntu',
+      		'mode'  => '644',       		
+      )
+      
+      
+      should contain_file('/etc/pamd.d/login')
+      should contain_file('/etc/securetty')
+      
+      # TODO: SSH config
+     end
+  end
+
+end
+
